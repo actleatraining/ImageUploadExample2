7ac727a24248dcaf18f856e6f9c8bc11702725f7
Andreas Olsson <andreas@actlea.com>
2024-11-24 08:00:25 +0300

    initial commit

---
+++ src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -0,0 +1,55 @@
+package com.example.uploadingfiles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.uploadingfiles.storage.StorageFileNotFoundException;
+import com.example.uploadingfiles.storage.StorageService;
+
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
+@RestController
+public class FileUploadController {
+
+	private final StorageService storageService;
+
+	@Autowired
+	public FileUploadController(StorageService storageService) {
+		this.storageService = storageService;
+	}
+
+	@GetMapping("/files/{filename}")
+	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+		Resource file = storageService.loadAsResource(filename);
+
+		if (file == null)
+			return ResponseEntity.notFound().build();
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.contentType(MediaType.valueOf(IMAGE_PNG_VALUE))
+				.body(file);
+	}
+
+	@PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+
+		storageService.store(file);
+		return "You successfully uploaded " + file.getOriginalFilename() + "!";
+	}
+
+	@ExceptionHandler(StorageFileNotFoundException.class)
+	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+		return ResponseEntity.notFound().build();
+	}
+}
